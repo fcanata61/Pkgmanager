659742ca5008d564814b9cf1f001c31e9c98ebef
fcanata61 <fernandocanatta26@gmail.com>
2025-09-05 17:37:17 -0300

    Update build.sh

--- packages/hello/build.sh
+++ packages/hello/build.sh
@@ -4,7 +4,6 @@ set -e
 # Cores ANSI
 GREEN="\033[1;32m"
 RED="\033[1;31m"
-YELLOW="\033[1;33m"
 BLUE="\033[1;34m"
 CYAN="\033[1;36m"
 RESET="\033[0m"
@@ -12,13 +11,11 @@ RESET="\033[0m"
 highlight() { echo -e "${CYAN}$1${RESET}"; }
 log_step() { echo -e "${BLUE}>>>${RESET} $1"; }
 log_ok() { echo -e "[${GREEN}+${RESET}] $1"; }
-log_err() { echo -e "[${RED}-${RESET}] $1"; }
 
 . ./metadata
 
 BUILD_DIR=$(pwd)/build
 PKG_DIR=$(pwd)/pkg
-
 rm -rf $BUILD_DIR $PKG_DIR
 mkdir -p $BUILD_DIR $PKG_DIR
 
@@ -34,7 +31,17 @@ log_step "Configurando build"
 ./configure --prefix=/usr
 
 log_step "Compilando $(highlight $NAME $VERSION)"
-make -j$(nproc)
+
+# Barra de progresso aproximada
+TOTAL=$(make -n | wc -l)
+COUNT=0
+make -j$(nproc) 2>&1 | while read LINE; do
+    COUNT=$((COUNT+1))
+    PERCENT=$((COUNT*100/TOTAL))
+    BAR=$(printf "%-${PERCENT}s" "#" | tr ' ' '#')
+    echo -ne "[${highlight $NAME}] ${PERCENT}% [${BAR}]\r"
+done
+echo -e "\n"
 
 log_step "Instalando em diretório temporário"
 make DESTDIR=$PKG_DIR install
